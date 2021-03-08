64663e9adc232ff0adc59015ee7de68406ff2c47
Paul <me@paulbill.com>
2021-03-08 15:37:39 +0000

    Add rule for import/extensions

--- base.js
+++ base.js
@@ -29,5 +29,13 @@ module.exports = {
     // Sort keys alphabetically - warning only as erroring will cause a lot of problems for mature apps.
     // Good habit to have though, makes working with large objects/data structures easier IMO
     'sort-keys': 'warn',
+
+    // For some reason this is causing an error in react projects, adding it here evern though it should be covered by
+    // Airbnb's own config.
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'never',
+      mjs: 'never',
+      jsx: 'never',
+    }],
   },
 };
