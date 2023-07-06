bc8adfc9b446badd97c4cf67e5ac58f5cd44cec8
William Cardoso <wcardosodev@hotmail.com>
2023-07-06 13:57:21 +0100

    pull in eslint-plugin-react

--- default.js
+++ default.js
@@ -4,7 +4,7 @@ module.exports = {
     jest: true,
   },
   extends: ['prettier'],
-  plugins: ['prettier'],
+  plugins: ['eslint-plugin-react', 'prettier'],
   rules: {
     // For some reason this is causing an error in react projects, adding it here even though it should be covered by
     // Airbnb's own config.
@@ -17,15 +17,6 @@ module.exports = {
         mjs: 'never',
         ts: 'never',
         tsx: 'never',
-      },
-    ],
-
-    'import/no-import-module-exports': 'warn',
-    'import/no-relative-packages': 'warn',
-    'react/require-default-props': [
-      'error',
-      {
-        functions: 'defaultArguments',
       },
     ],
 
@@ -49,6 +40,13 @@ module.exports = {
       },
     ],
 
+    'react/require-default-props': [
+      'error',
+      {
+        functions: 'defaultArguments',
+      },
+    ],
+
     // Sort keys alphabetically - warning only as erroring will cause a lot of problems for mature apps.
     // Good habit to have though, makes working with large objects/data structures easier IMO
     'sort-keys': 'warn',
@@ -58,6 +56,9 @@ module.exports = {
       node: {
         extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       },
+    },
+    react: {
+      version: 'detect',
     },
   },
 };
